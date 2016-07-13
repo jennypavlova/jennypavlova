156e8e9a02bc5a672f750896f8fb9452f6fff325
Jenny Pavlova <jennypavlova94@gmail.com>
2016-07-13 18:01:41 +0300

    landing page redesign

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+// Scroll from home section to next
+$('#button-education').click(function(){
+  $('html, body').animate({
+      scrollTop: $("#education").offset().top
+  }, 1000);
+});
+//Scroll from education section to next
+$('#button-courses').click(function(){
+  $('html, body').animate({
+      scrollTop: $("#courses").offset().top
+  }, 1000);
+});
