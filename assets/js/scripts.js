d1479ba1349447f91cea77b4c6e5fa87a6cb67cd
Jenny Pavlova <jennypavlova94@gmail.com>
2016-07-14 17:57:53 +0300

    Background and color fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,11 @@ $('#button-education').click(function(){
       scrollTop: $("#education").offset().top
   }, 1000);
 });
+$('#button-education-slide').click(function(){
+  $('html, body').animate({
+      scrollTop: $("#education").offset().top
+  }, 1000);
+});
 //Scroll from education section to next
 $('#button-courses').click(function(){
   $('html, body').animate({
